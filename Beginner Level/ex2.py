f9211160cabebe8d07d5a2f595eedd4e4ddb09db
davishrahul <36623942+davishrahul@users.noreply.github.com>
2018-05-16 12:04:04 +0530

    Update ex2.py

--- Beginner Level/ex2.py
+++ Beginner Level/ex2.py
@@ -1,7 +1,7 @@
-a=int(input())
-if (a%2)==0:
+b=int(input())
+if (b%2)==0:
      print ("Even")
-     elif (a%2)!==0:
+     elif (b%2)!==0:
           print ("Odd")
           else:
           print ("invalid output")
